17e157f8b1d330961090f21e9e3c38b75d5c16ea
Adithya SM <63196135+Adithya65@users.noreply.github.com>
2021-08-02 23:03:36 +0530

    Update Obj_LK.py

--- Obj_LK.py
+++ Obj_LK.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[19]:
-
-
 def onMouse(event,x,y,f,p):
     global initial_y,initial_x,k
     if event ==cv2.EVENT_LBUTTONDOWN:
@@ -18,7 +14,7 @@ initial_y,initial_x,k=0,0,1
 cv2.namedWindow("window")
 cv2.setMouseCallback("window",onMouse)
 cap=cv2.VideoCapture(0)
-old_pts=np.array([[ix,iy]],dtype="float32").reshape(-1,1,2)
+old_pts=np.array([[initial_x,initial_y]],dtype="float32").reshape(-1,1,2)
 while True :
     _,frame=cap.read()
     cv2.imshow("window",frame)
@@ -28,13 +24,15 @@ while True :
         cv2.destroyAllWindows()
         break
 cap1=cv2.VideoCapture(0)  
-old_pts=np.array([[ix,iy]],dtype="float32").reshape(-1,1,2)
-mask=np.zeros_like(frame2)
+old_pts=np.array([[initial_x,initial_y]],dtype="float32").reshape(-1,1,2)
+mask=np.zeros_like(frame)
 while True:
     _,frame2=cap1.read()
     new_gray=cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
-    new_pts,status,err=cv2.calcOpticalFlowPyrLK(old_gray,new_gray,old_pts,None,maxLevel=1,criteria=(cv2.TERM_CRITERIA_EPS|cv2.TERM_CRITERIA_COUNT,15,0.008))
-    cv2.circle(mask,(new_pts.ravel()[0],new_pts.ravel()[1]),2,(0,255,0),2)
+    new_pts,status,err=cv2.calcOpticalFlowPyrLK(old_gray,new_gray,old_pts,None,maxLevel=1,criteria=(1|2,15,0.008))
+    bn=int(new_pts.ravel()[0])
+    cn=int(new_pts.ravel()[1])
+    cv2.circle(mask,(bn,cn),8,(0,0,255),-1)
     comvined=cv2.addWeighted(frame2,0.7,mask,0.3,0.1)
     cv2.imshow("new win",mask)
     cv2.imshow("win",comvined)
@@ -46,4 +44,3 @@ while True:
         cap1.release()
         cv2.destroyAllWindows()
         break
-
